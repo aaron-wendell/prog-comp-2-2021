32f61da33de6109d132fb768edc7db76fb3cb172
Aaron <aaronmoreiracampos@gmail.com>
2021-08-24 21:57:36 -0300

    24-08

--- vetor/js/vetorobj.js
+++ vetor/js/vetorobj.js
@@ -40,23 +40,94 @@ let gerenciaredessociais = () => {
     }
     //ex1 o usuario informa a rede social e o programa retorna quantos posts foram feitos
 
-    let function(){
-
-    vetorredessociais.push(nome.objeto)}
-
-    while(!achou && j<5)
-    {
-        j == usuario
-
-
-
-
-
+    let codigo = Number(prompt(`Informe o codigo da rede social`))
+    //percorre vetor procurando
+    let conta = 0   
+    for(let i=0;i<5; i++)
+    {   // percorre o vetor de usuarios
+        if(vetorusuarios[i].codigoredesocial == codigo)
+        {
+            conta = conta + vetorusuarios[i].qtdeposts
+        }
     }
+    if(conta == 0)
+    {
+        console.log(`Nao houve postagem ou rede social nao existe`)
+    }
+    else{
+        console.log(`A quantidade de posts na rede social ${codigo} foi ${conta}`)
+    }
+
     //ex2 o programa retorna quantos posts foram feitos em todas as redes sociais
 
+    for(let i=0; i<5; i++){ //para cada rede social
+        let post = 0
+        for(let j=0; j<5; j++)
+        {   //procura usuarios
+            if(vetorredessociais[i].codigo == vetorusuarios[i].codigoredesocial)
+            {
+                post = post + vetorusuarios[i].qtdeposts
+            }
+        }
+        // terminou soma dos posts de uma rede social
+        console.log(`A quantidade de posts da rede social ${vetorredessociais[i].nome} foi ${posts}`)
+    }
+    if(post == 0)
+    {
+        console.log(`Nao houve postagem nas redes sociais`)
+    }
+    
+
     //ex3 o usuario informa o login e o programa retorna quantos posts foram feitos
+    let login = prompt(`Digite o login `)
+    // procura o login no vetor de usuarios
+    let somapost = 0
+    let achou = false // nao encontrei o usuario
+    for(let i=0; i<5; i++)
+    {   
+        if(vetorusuarios[i].login == login)
+        {
+            somapost = somapost + vetorusuarios[i].qtdeposts
+            achou = true
+        }
+        
+    }
+    if (achou == true)
+    {
+        alert(`O usuario ${nome} realizou ${somapost}`)
+    }
+    else{
+        alert(`Usuario nao encontrado`)
+    }
 
     //ex4 o programa retorna quantos posts foram feitos por cada usuario
-
+    
+    let vetorusuariospost = []
+        for(let i=0; i<5; i++)
+        {   
+            for(let j=0; j<vetorusuariospost.length; j++)
+            {
+                if(vetorusuarios[i].login == vetorusuariospost[i].login)
+                {
+                    //soma qtde posts do usuario
+                    vetorusuariospost[j].qtde = vetorusuariospost[j].qtde + vetorusuarios[i].qtdeposts
+                    achou = true
+                }
+            }
+        }
+        //usuario ainda nao existe no vetorusuariospost, ent vamos criar
+        if(!achou)
+        {   //criar usuario no vetor
+            vetorusuariospost.push({
+                login: vetorusuarios[i].login,
+                qtde: vetorusuarios[i].qtdeposts
+            })
+        }
+        console.log(vetorusuariospost)
+        for(i=0; i<5; i++)
+        {
+            console.log(`A quantidade de posts de ${vetorusuarios[i].nome} é ${post}`)
+        vetorusuarios.push(objeto.nome)
+        vetorusuarios.push(objeto.qtdeposts)
+        }
 }
